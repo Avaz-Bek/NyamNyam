97b2d7704ae3c1f1fc8622ccd5b6c1dae636b9a9
Atay Sultangaziev <horryworry@gmail.com>
2019-02-20 09:15:55 +0600

    first commit

---
+++ NyamNyam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NyamNyam
+//
+//  Created by Atay Sultangaziev on 2/20/19.
+//  Copyright © 2019 Atay Sultangaziev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
