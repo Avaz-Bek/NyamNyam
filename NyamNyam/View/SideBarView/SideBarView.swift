c4010f914f2ba808306c045ac7aff81e95e02818
Avazbek Kodiraliev <avazbek@macbook-pro-avazbek.local>
2019-02-22 11:56:10 +0600

    SideBarMenu

--- NyamNyam/View/SideBarView/SideBarView.swift
+++ NyamNyam/View/SideBarView/SideBarView.swift
@@ -15,23 +15,27 @@ protocol SidebarViewDelegate: class {
 
 enum Row: String {
     case editProfile
-    case messages
+    case menu
     case contact
-    case settings
-    case history
-    case help
+    case client_map
+    case our_network
+    case reservation
+    case call
+    case stock
     case signOut
     case none
     
     init(row: Int) {
         switch row {
         case 0: self = .editProfile
-        case 1: self = .messages
+        case 1: self = .menu
         case 2: self = .contact
-        case 3: self = .settings
-        case 4: self = .history
-        case 5: self = .help
-        case 6: self = .signOut
+        case 3: self = .client_map
+        case 4: self = .our_network
+        case 5: self = .reservation
+        case 6: self = .call
+        case 7: self = .stock
+        case 8: self = .signOut
         default: self = .none
         }
     }
@@ -45,10 +49,11 @@ class SidebarView: UIView, UITableViewDelegate, UITableViewDataSource {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor=UIColor(red: 54/255, green: 55/255, blue: 56/255, alpha: 1.0)
+        //self.backgroundColor=UIColor(red: 51/255, green: 58/255, blue: 149/255, alpha: 1.0)
+        self.backgroundColor=UIColor.clear
         self.clipsToBounds=true
         
-        titleArr = ["Akhilendra Singh", "Messages", "Contact", "Settings", "History", "Help", "Sign Out"]
+        titleArr = ["Анна", "Меню", "Контакты", "Карта клиента", "Наша сеть", "Заказ столика", "Позвонить","Акции","Войти","О нас"]
         
         setupViews()
         
@@ -56,11 +61,12 @@ class SidebarView: UIView, UITableViewDelegate, UITableViewDataSource {
         myTableView.dataSource=self
         myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
         myTableView.tableFooterView=UIView()
-        myTableView.separatorStyle = UITableViewCell.SeparatorStyle.none
         myTableView.allowsSelection = true
-        myTableView.bounces=false
-        myTableView.showsVerticalScrollIndicator=false
-        myTableView.backgroundColor = UIColor.clear
+        myTableView.bounces = false
+        myTableView.showsVerticalScrollIndicator = false
+        myTableView.backgroundColor = UIColor.white
+        
+        self.myTableView.register(UINib(nibName: "SideBarTableViewCell", bundle: nil), forCellReuseIdentifier: "SideBarTableViewCell")
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -68,28 +74,35 @@ class SidebarView: UIView, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell=tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let cell=tableView.dequeueReusableCell(withIdentifier: "SideBarTableViewCell", for: indexPath) as! SideBarTableViewCell
         cell.backgroundColor = .clear
         cell.selectionStyle = .none
         if indexPath.row == 0 {
-            cell.backgroundColor=UIColor(red: 77/255, green: 77/255, blue: 77/255, alpha: 1.0)
+            //cell.backgroundColor=UIColor(red: 77/255, green: 77/255, blue: 77/255, alpha: 1.0)
+            cell.backgroundColor=UIColor.clear
             let cellImg: UIImageView!
             cellImg = UIImageView(frame: CGRect(x: 15, y: 10, width: 80, height: 80))
             cellImg.layer.cornerRadius = 40
-            cellImg.layer.masksToBounds=true
+            cellImg.layer.masksToBounds = true
             cellImg.contentMode = .scaleAspectFill
-            cellImg.layer.masksToBounds=true
-            cellImg.image=#imageLiteral(resourceName: "user")
+            cellImg.layer.masksToBounds = true
+            cellImg.image = #imageLiteral(resourceName: "user.jpeg")
             cell.addSubview(cellImg)
             
             let cellLbl = UILabel(frame: CGRect(x: 110, y: cell.frame.height/2-15, width: 250, height: 30))
             cell.addSubview(cellLbl)
             cellLbl.text = titleArr[indexPath.row]
-            cellLbl.font=UIFont.systemFont(ofSize: 17)
-            cellLbl.textColor=UIColor.white
+            cellLbl.font=UIFont.systemFont(ofSize: 25)
+            cellLbl.textColor=UIColor.black
         } else {
-            cell.textLabel?.text=titleArr[indexPath.row]
-            cell.textLabel?.textColor=UIColor.white
+            cell.nameLabel.text = titleArr[indexPath.row]
+            cell.backgroundColor = UIColor.clear
+            cell.nameLabel.textColor=UIColor.black
+            cell.sideImageView.image = #imageLiteral(resourceName: "user.jpeg")
+            cell.sideImageView.layer.cornerRadius = 10
+            cell.sideImageView.layer.masksToBounds = true
+            cell.sideImageView.contentMode = .scaleAspectFill
+            cell.sideImageView.layer.masksToBounds = true
         }
         return cell
     }
--- NyamNyam/View/Start/StartViewController.swift
+++ NyamNyam/View/Start/StartViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class StartViewController: UIViewController {
 
+    var sidebarView: SidebarView!
+    var blackScreen: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,10 +21,79 @@ class StartViewController: UIViewController {
         view?.backgroundColor = UIColor(white: 0.95, alpha: 1)
         
         
-
+        let btnMenu = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_menu_white_3x"), style: .plain, target: self, action: #selector(btnMenuAction))
+        //btnMenu.tintColor = UIColor(red: 54/255, green: 55/255, blue: 56/255, alpha: 1.0)
+        btnMenu.tintColor = UIColor.white
+        self.navigationItem.leftBarButtonItem = btnMenu
         
+        sidebarView=SidebarView(frame: CGRect(x: 0, y: 0, width: 0, height: self.view.frame.height))
+        sidebarView.delegate = self as SidebarViewDelegate
+        sidebarView.layer.zPosition = 100
+        self.view.isUserInteractionEnabled = true
+        self.navigationController?.view.addSubview(sidebarView)
+        
+        blackScreen=UIView(frame: self.view.bounds)
+        blackScreen.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        blackScreen.isHidden = true
+        self.navigationController?.view.addSubview(blackScreen)
+        blackScreen.layer.zPosition = 99
+        let tapGestRecognizer = UITapGestureRecognizer(target: self, action: #selector(blackScreenTapAction(sender:)))
+        blackScreen.addGestureRecognizer(tapGestRecognizer)
+        
+        
+    }
+    
+    @objc func btnMenuAction() {
+        blackScreen.isHidden = false
+        UIView.animate(withDuration: 0.3, animations: {
+            self.sidebarView.frame = CGRect(x: 0, y: 0, width: 250, height: self.sidebarView.frame.height)
+        }) { (complete) in
+            self.blackScreen.frame=CGRect(x: self.sidebarView.frame.width, y: 0, width: self.view.frame.width-self.sidebarView.frame.width, height: self.view.bounds.height+100)
+        }
+    }
+    
+    @objc func blackScreenTapAction(sender: UITapGestureRecognizer) {
+        blackScreen.isHidden = true
+        blackScreen.frame = self.view.bounds
+        UIView.animate(withDuration: 0.3) {
+            self.sidebarView.frame = CGRect(x: 0, y: 0, width: 0, height: self.sidebarView.frame.height)
+        }
     }
 
 
 }
 
+
+extension StartViewController: SidebarViewDelegate {
+    func sidebarDidSelectRow(row: Row) {
+        blackScreen.isHidden = true
+        blackScreen.frame = self.view.bounds
+        UIView.animate(withDuration: 0.3) {
+            self.sidebarView.frame=CGRect(x: 0, y: 0, width: 0, height: self.sidebarView.frame.height)
+        }
+        switch row {
+        case .editProfile:
+            print("Изменить профиль")
+        case .menu:
+            print("Меню")
+        case .contact:
+            print("Контакты")
+        case .client_map:
+            print("Карта клиента")
+        case .our_network:
+            print("Наша сеть")
+        case .reservation:
+            print("Заказ столика")
+        case .call:
+            print("Позвонить")
+        case .stock:
+            print("Акции")
+        case .signOut:
+            print("Войти")
+        case .none:
+            break
+            //        default:  //Default will never be executed
+            //            break
+        }
+    }
+}
